91fe61e0bab6fed30bca893b401c0ead4442236c
simonLab <simelav24@gmail.com>
2021-05-31 17:04:30 +0300

    finished

--- db/ip/sld6dfc5e3d/submodules/alt_sld_fab_alt_sld_fab_ident.sv
+++ db/ip/sld6dfc5e3d/submodules/alt_sld_fab_alt_sld_fab_ident.sv
@@ -20,7 +20,7 @@
 // Altera Identification hub
 //
 // Parameters
-//   DESIGN_HASH   : 1ed822d650953cd6ed43
+//   DESIGN_HASH   : 07e02c16f7a3e216f770
 //   COUNT         : 1
 //   ROM_WIDTHS    : 4
 //   LATENCIES     : 0
@@ -39,7 +39,7 @@ module alt_sld_fab_alt_sld_fab_ident
 );
 
 
-wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h1ed822d650953cd6ed43 };
+wire [127:0] data_0 = { mixed, 12'h0, 32'h0, 80'h07e02c16f7a3e216f770 };
 
 reg [3:0] result_0;
 always @(address_0 or data_0) begin
